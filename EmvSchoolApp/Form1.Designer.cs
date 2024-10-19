b15bdff893e3c561b81a88aabc8b4172e9bf0369
Левон Туманян <levonchik2000@gmail.com>
2024-10-19 16:08:49 +0300

    Добавьте файлы проекта.

---
+++ EmvSchoolApp/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace EmvSchoolApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			button1 = new Button();
+			button2 = new Button();
+			label1 = new Label();
+			textBox1 = new TextBox();
+			textBox2 = new TextBox();
+			label2 = new Label();
+			button3 = new Button();
+			progressBar1 = new ProgressBar();
+			checkBox1 = new CheckBox();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(423, 48);
+			button1.Margin = new Padding(3, 4, 3, 4);
+			button1.Name = "button1";
+			button1.Size = new Size(86, 31);
+			button1.TabIndex = 0;
+			button1.Text = "Обзор";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// button2
+			// 
+			button2.Location = new Point(423, 123);
+			button2.Margin = new Padding(3, 4, 3, 4);
+			button2.Name = "button2";
+			button2.Size = new Size(86, 31);
+			button2.TabIndex = 1;
+			button2.Text = "Обзор";
+			button2.UseVisualStyleBackColor = true;
+			button2.Click += button2_Click;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(14, 24);
+			label1.Name = "label1";
+			label1.Size = new Size(197, 20);
+			label1.TabIndex = 2;
+			label1.Text = "Выберите файл для чтения";
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(14, 48);
+			textBox1.Margin = new Padding(3, 4, 3, 4);
+			textBox1.Name = "textBox1";
+			textBox1.ReadOnly = true;
+			textBox1.Size = new Size(413, 27);
+			textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			textBox2.Location = new Point(14, 123);
+			textBox2.Margin = new Padding(3, 4, 3, 4);
+			textBox2.Name = "textBox2";
+			textBox2.ReadOnly = true;
+			textBox2.Size = new Size(413, 27);
+			textBox2.TabIndex = 4;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(14, 99);
+			label2.Name = "label2";
+			label2.Size = new Size(237, 20);
+			label2.TabIndex = 5;
+			label2.Text = "Выберете место для сохранения";
+			// 
+			// button3
+			// 
+			button3.Location = new Point(138, 161);
+			button3.Margin = new Padding(3, 4, 3, 4);
+			button3.Name = "button3";
+			button3.Size = new Size(145, 31);
+			button3.TabIndex = 6;
+			button3.Text = "Конвертировать";
+			button3.UseVisualStyleBackColor = true;
+			button3.Click += button3_Click;
+			// 
+			// progressBar1
+			// 
+			progressBar1.Location = new Point(14, 200);
+			progressBar1.Margin = new Padding(3, 4, 3, 4);
+			progressBar1.Name = "progressBar1";
+			progressBar1.Size = new Size(414, 31);
+			progressBar1.TabIndex = 7;
+			// 
+			// checkBox1
+			// 
+			checkBox1.AutoSize = true;
+			checkBox1.Location = new Point(14, 249);
+			checkBox1.Name = "checkBox1";
+			checkBox1.Size = new Size(101, 24);
+			checkBox1.TabIndex = 8;
+			checkBox1.Text = "checkBox1";
+			checkBox1.UseVisualStyleBackColor = true;
+			checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(914, 600);
+			Controls.Add(checkBox1);
+			Controls.Add(progressBar1);
+			Controls.Add(button3);
+			Controls.Add(label2);
+			Controls.Add(textBox2);
+			Controls.Add(textBox1);
+			Controls.Add(label1);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Margin = new Padding(3, 4, 3, 4);
+			Name = "Form1";
+			Text = "Form1";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button button1;
+		private Button button2;
+		private Label label1;
+		private TextBox textBox1;
+		private TextBox textBox2;
+		private Label label2;
+		private Button button3;
+		private ProgressBar progressBar1;
+		private CheckBox checkBox1;
+	}
+}
---
+++ EmvSchoolApp/Form1.cs
@@ -0,0 +1,163 @@
+namespace EmvSchoolApp
+{
+	public partial class Form1 : Form
+	{
+		private string sourceFilePath;
+		private string targetFilePath;
+		private string connectionString = "data source=cl-mssql-1-ag,1433;initial catalog=StudentCard;persist security info=True;user id=Checks;password=checks;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+		private const string filePath = "savedText.txt";
+		public Form1()
+		{
+			InitializeComponent();
+			LoadText();
+		}
+		private void LoadText()
+		{
+			if (File.Exists(filePath))
+			{
+				// Загружаем текст из файла
+				textBox2.Text = File.ReadAllText(filePath);
+				targetFilePath = textBox2.Text;
+				checkBox1.Checked = true; // Устанавливаем чекбокс в состояние "отмечен"
+			}
+		}
+		private void SaveText(string text)
+		{
+			// Сохраняем текст в файл
+			File.WriteAllText(filePath, text);
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog openFileDialog = new OpenFileDialog())
+			{
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					textBox1.Text = openFileDialog.FileName;
+					sourceFilePath = openFileDialog.FileName;
+					MessageBox.Show("Выбран файл: " + sourceFilePath);
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(sourceFilePath))
+			{
+				MessageBox.Show("Сначала выберите файл для чтения.");
+				return;
+			}
+			using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+			{
+				if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+				{
+					targetFilePath = folderBrowserDialog.SelectedPath;
+					textBox2.Text = targetFilePath.ToString();
+
+				}
+			}
+		}
+		private bool IsFileLocked(string filePath)
+		{
+			try
+			{
+				using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
+				{
+					return false; // Файл доступен
+				}
+			}
+			catch (IOException)
+			{
+				return true; // Файл заблокирован
+			}
+		}
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(targetFilePath))
+			{
+				MessageBox.Show("Сначала выберите путь сохранения файла.");
+				return;
+			}
+
+			if (IsFileLocked(sourceFilePath))
+			{
+				MessageBox.Show("Файл заблокирован другим процессом. Пожалуйста, закройте его и попробуйте снова.");
+				return;
+			}
+
+			try
+			{
+				string fileName = Path.GetFileName(sourceFilePath);
+				string destinationFilePath = Path.Combine(targetFilePath, fileName);
+
+				// Проверяем, существует ли файл в целевой папке
+				if (File.Exists(destinationFilePath))
+				{
+					// Если файл существует, создаем новое имя для копии
+					string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(sourceFilePath);
+					string fileExtension = Path.GetExtension(sourceFilePath);
+					int copyNumber = 1;
+
+					// Генерируем новое имя файла, пока не найдем доступное
+					do
+					{
+						string newFileName = $"{fileNameWithoutExtension}_copy{copyNumber}{fileExtension}";
+						destinationFilePath = Path.Combine(targetFilePath, newFileName);
+						copyNumber++;
+					} while (File.Exists(destinationFilePath));
+				}
+
+				// Сбрасываем прогресс бар
+				progressBar1.Value = 0;
+				progressBar1.Visible = true;
+
+				// Копируем файл с обновлением прогресс бара
+				using (FileStream sourceStream = new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read))
+				using (FileStream destinationStream = new FileStream(destinationFilePath, FileMode.Create, FileAccess.Write))
+				{
+					byte[] buffer = new byte[4096]; // Буфер для чтения
+					long totalBytes = sourceStream.Length;
+					long totalReadBytes = 0;
+					int bytesRead;
+
+					while ((bytesRead = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+					{
+						destinationStream.Write(buffer, 0, bytesRead);
+						totalReadBytes += bytesRead;
+
+						// Обновляем прогресс бар
+						int progressPercentage = (int)((totalReadBytes * 100) / totalBytes);
+						progressBar1.Value = progressPercentage;
+					}
+				}
+
+				MessageBox.Show("Файл успешно скопирован в: " + destinationFilePath);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Ошибка при копировании файла: " + ex.Message);
+			}
+			finally
+			{
+				// Скрываем прогресс бар после завершения
+				progressBar1.Visible = false;
+			}
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked)
+			{
+				// Сохраняем текст в файл
+				SaveText(textBox2.Text);
+			}
+			else
+			{
+				// Удаляем файл, если чекбокс снят
+				if (File.Exists(filePath))
+				{
+					File.Delete(filePath);
+				}
+			}
+		}
+	}
+}
